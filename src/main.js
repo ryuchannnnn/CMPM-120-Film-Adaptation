57015011ff73b650a9a6913e92dd9e8f3e1b088b
wiwechen <129801256+wiwechen@users.noreply.github.com>
2023-06-10 23:48:36 -0700

    put comment box in main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,28 @@
+/**********************************************************************************************
+ * Name: William Chen and Danny Chen
+ * Major Components Used:
+ * Physics System
+ * Text Objects
+ * The Animation Mangager
+ * Tilemaps
+ * Timers
+ * 
+ * Features:
+ * You might notice that our game has no camera movement, as well as minamal instructions and sound effects in our game.
+ * This was our creative way on adapating the style of filming of this movie. This movie primarly films in static camera,
+ * meaning the camera is not moved at all. No shakes or cuts. And where there is movement with the camera, it's the emphasis 
+ * the impact of the current scene. The movie also has a lot of scenes of where a character in the movie will either just be standing
+ * around or walking around in place without saying a word. We adapted this aspects of these types of scene by giving players the ability
+ * to roam around the area they're currently at until they complete the action of the scenes. As for the no sounds, the only sound effect in
+ * this game is when you continue through the dialouge box or when you do certain actions in the game.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
 let config = {
     type: Phaser.CANVAS,
     render: {
