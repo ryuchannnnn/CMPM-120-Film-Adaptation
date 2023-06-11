4a7b99878d6f09d00ea1b35ff6590390076b068e
Danny C <dchan27@ucsc.edu>
2023-06-10 22:41:39 -0700

    menu and credits up

--- src/scenes/credits.js
+++ src/scenes/credits.js
@@ -2,12 +2,19 @@ class Credits extends Phaser.Scene{
     constructor(){
         super({key: 'creditsScene'})
     }
-    create(){
-        this.add.text(20, 20, "Credits Scene");
-        this.add.text()
+    create()
+    {
+        this.add.tileSprite(0,0,480,480,'creditBackground').setOrigin(0,0);
 
-
-        this.add.text(20, 40, "Press Space to go back to menu");
+        this.add.text(0,100, "Room assets: https://opengameart.org/\ncontent/lpc-house-insides");
+        this.add.text(0,140, "Grave assets: https://opengameart.org/\ncontent/lpc-grave-markers-remix");
+        this.add.text(0,180, "Outdoor assets: https://opengameart.org/\ncontent/16x16-overworld-tiles");
+        this.add.text(0,220, "Dad sprite: https://www.deviantart.com/\nmaicerochico/art/Platinum-Looker-Overworld-\nSprites-Ripped-651961803 (recolored by Danny)");
+        this.add.text(0,270, "Son sprite: https://www.deviantart.com/\nteraneck/art/Youngster-BW-gen3-856433474 \n(recolored by Danny)");
+        this.add.text(0,320, "Daughter sprite: https://www.deviantart.com/\nteraneck/art/Cassidy-856436695 \n(recolored by Danny)");
+        this.add.text(0,370, "button sound: https://opengameart.org/content/\nbotton-sound-pack");
+        this.add.text(0,410, "mother and gradnma sprites were drawn by Danny");
+        this.add.text(0,450, "Press Space to go back to menu");
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
     }
--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -4,16 +4,11 @@ class Menu extends Phaser.Scene{
     }
 
     create(){
+        this.add.tileSprite(0,0,480,480,'menuBackground').setOrigin(0,0);
         console.log("At Menu");
-        this.add.text(20, 20, "Menu Screen");
         // let test = this.add.rectangle(centerX, centerY, 30, 30, 0xff0000);
         // let test1 = this.add.rectangle(centerX-50, centerY, 40, 40, 0xff0000);
         // let test2 = this.add.rectangle(centerX+50, centerY, 20, 20, 0xff0000);
-
-        this.add.text(20, 40, "Click P to start Playing");
-        this.add.text(20, 60, "Click Z to open Rules");
-        this.add.text(20, 80, "Click X to open Credits");
-
         // key control used to navigate through the menu
         keyP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
         keyZ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
